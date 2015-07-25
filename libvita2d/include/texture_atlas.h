8da6a0e17e8085213f9171fa2a4df64021a128ae
Sergi Granell <xerpi.g.12@gmail.com>
2015-07-25 12:54:29 +0200

    texture_atlas: use int_htab

--- libvita2d/include/texture_atlas.h
+++ libvita2d/include/texture_atlas.h
@@ -3,32 +3,31 @@
 
 #include "vita2d.h"
 #include "bin_packing_2d.h"
+#include "int_htab.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef struct ascii_table_entry {
+typedef struct atlas_htab_entry {
 	bp2d_rectangle rect;
 	int bitmap_left;
 	int bitmap_top;
 	int advance_x;
 	int advance_y;
-	int used;
-} ascii_table_entry;
+} atlas_htab_entry;
 
 typedef struct texture_atlas {
 	vita2d_texture *tex;
 	bp2d_node *bp_root;
-	/* Crappy ASCII cache */
-	ascii_table_entry ascii_table[128];
+	int_htab *htab;
 } texture_atlas;
 
 texture_atlas *texture_atlas_create(int width, int height);
 void texture_atlas_free(texture_atlas *atlas);
-int texture_atlas_insert(texture_atlas *atlas, unsigned char character, const void *image, int width, int height, int bitmap_left, int bitmap_top, int advance_x, int advance_y);
-int texture_atlas_exists(texture_atlas *atlas, unsigned char character);
-void texture_atlas_get(texture_atlas *atlas, unsigned char character, bp2d_rectangle *rect, int *bitmap_left, int *bitmap_top, int *advance_x, int *advance_y);
+int texture_atlas_insert(texture_atlas *atlas, unsigned int character, const void *image, int width, int height, int bitmap_left, int bitmap_top, int advance_x, int advance_y);
+int texture_atlas_exists(texture_atlas *atlas, unsigned int character);
+void texture_atlas_get(texture_atlas *atlas, unsigned int character, bp2d_rectangle *rect, int *bitmap_left, int *bitmap_top, int *advance_x, int *advance_y);
 
 #ifdef __cplusplus
 }
--- libvita2d/source/texture_atlas.c
+++ libvita2d/source/texture_atlas.c
@@ -16,7 +16,7 @@ texture_atlas *texture_atlas_create(int width, int height)
 
 	atlas->tex = vita2d_create_empty_texture(width, height);
 	atlas->bp_root = bp2d_create(&rect);
-	memset(atlas->ascii_table, 0, sizeof(atlas->ascii_table));
+	atlas->htab = int_htab_create();
 
 	return atlas;
 }
@@ -25,10 +25,11 @@ void texture_atlas_free(texture_atlas *atlas)
 {
 	vita2d_free_texture(atlas->tex);
 	bp2d_free(atlas->bp_root);
+	int_htab_free(atlas->htab);
 	free(atlas);
 }
 
-int texture_atlas_insert(texture_atlas *atlas, unsigned char character, const void *image, int width, int height, int bitmap_left, int bitmap_top, int advance_x, int advance_y)
+int texture_atlas_insert(texture_atlas *atlas, unsigned int character, const void *image, int width, int height, int bitmap_left, int bitmap_top, int advance_x, int advance_y)
 {
 	bp2d_size size;
 	size.w = width;
@@ -38,15 +39,18 @@ int texture_atlas_insert(texture_atlas *atlas, unsigned char character, const vo
 	if (bp2d_insert(atlas->bp_root, &size, &pos) == 0)
 		return 0;
 
-	atlas->ascii_table[character].rect.x = pos.x;
-	atlas->ascii_table[character].rect.y = pos.y;
-	atlas->ascii_table[character].rect.w = width;
-	atlas->ascii_table[character].rect.h = height;
-	atlas->ascii_table[character].bitmap_left = bitmap_left;
-	atlas->ascii_table[character].bitmap_top = bitmap_top;
-	atlas->ascii_table[character].advance_x = advance_x;
-	atlas->ascii_table[character].advance_y = advance_y;
-	atlas->ascii_table[character].used = 1;
+	atlas_htab_entry *entry = malloc(sizeof(*entry));
+
+	entry->rect.x = pos.x;
+	entry->rect.y = pos.y;
+	entry->rect.w = width;
+	entry->rect.h = height;
+	entry->bitmap_left = bitmap_left;
+	entry->bitmap_top = bitmap_top;
+	entry->advance_x = advance_x;
+	entry->advance_y = advance_y;
+
+	int_htab_insert(atlas->htab, character, entry);
 
 	void *tex_data = vita2d_texture_get_datap(atlas->tex);
 	unsigned int tex_width = vita2d_texture_get_width(atlas->tex);
@@ -59,19 +63,21 @@ int texture_atlas_insert(texture_atlas *atlas, unsigned char character, const vo
 	return 1;
 }
 
-int texture_atlas_exists(texture_atlas *atlas, unsigned char character)
+int texture_atlas_exists(texture_atlas *atlas, unsigned int character)
 {
-	return atlas->ascii_table[character].used;
+	return int_htab_get(atlas->htab, character) != NULL;
 }
 
-void texture_atlas_get(texture_atlas *atlas, unsigned char character, bp2d_rectangle *rect, int *bitmap_left, int *bitmap_top, int *advance_x, int *advance_y)
+void texture_atlas_get(texture_atlas *atlas, unsigned int character, bp2d_rectangle *rect, int *bitmap_left, int *bitmap_top, int *advance_x, int *advance_y)
 {
-	rect->x = atlas->ascii_table[character].rect.x;
-	rect->y = atlas->ascii_table[character].rect.y;
-	rect->w = atlas->ascii_table[character].rect.w;
-	rect->h = atlas->ascii_table[character].rect.h;
-	*bitmap_left = atlas->ascii_table[character].bitmap_left;
-	*bitmap_top = atlas->ascii_table[character].bitmap_top;
-	*advance_x = atlas->ascii_table[character].advance_x;
-	*advance_y = atlas->ascii_table[character].advance_y;
+	atlas_htab_entry *entry = int_htab_get(atlas->htab, character);
+
+	rect->x = entry->rect.x;
+	rect->y = entry->rect.y;
+	rect->w = entry->rect.w;
+	rect->h = entry->rect.h;
+	*bitmap_left = entry->bitmap_left;
+	*bitmap_top = entry->bitmap_top;
+	*advance_x = entry->advance_x;
+	*advance_y = entry->advance_y;
 }
