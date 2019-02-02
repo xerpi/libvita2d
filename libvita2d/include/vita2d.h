73abbbe2a15a6c2c484b841503dc7fae31cde504
Thomas Perl <m@thp.io>
2019-02-02 12:43:16 +0100

    vita2d_draw_array: Draw colored polygons

--- libvita2d/include/vita2d.h
+++ libvita2d/include/vita2d.h
@@ -95,6 +95,7 @@ void vita2d_draw_pixel(float x, float y, unsigned int color);
 void vita2d_draw_line(float x0, float y0, float x1, float y1, unsigned int color);
 void vita2d_draw_rectangle(float x, float y, float w, float h, unsigned int color);
 void vita2d_draw_fill_circle(float x, float y, float radius, unsigned int color);
+void vita2d_draw_array(SceGxmPrimitiveType mode, const vita2d_color_vertex *vertices, size_t count);
 
 void vita2d_texture_set_alloc_memblock_type(SceKernelMemBlockType type);
 SceKernelMemBlockType vita2d_texture_get_alloc_memblock_type();
